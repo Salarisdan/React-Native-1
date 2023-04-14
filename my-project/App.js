3293e3856bb302954843beafc8bdf728bb8532c3
Salarisdan <salarisdan@gmail.com>
2023-04-14 12:40:18 +0300

    -

--- my-project/App.js
+++ my-project/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import RegistrationScreen from './Screens/RegistrationScreen';
-import LoginScreen from './Screens/LoginScreen';
+
+import React from "react";
+import { View,} from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
   return (
     <View>
-      <Text>Welcome to my app!</Text>
       <RegistrationScreen />
       <LoginScreen />
     </View>
